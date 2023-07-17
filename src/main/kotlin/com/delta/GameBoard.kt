cf4f43df08bc986062e352aee646edf33dfab7e3
Anton <a.d.ablov@gmail.com>
2023-07-17 18:38:54 +0200

    Create a prototype

---
+++ src/main/kotlin/com/delta/GameBoard.kt
@@ -0,0 +1,54 @@
+package com.delta
+
+import com.google.gson.Gson
+
+class GameBoard(val size: Int) {
+    val board: Array<Array<PlayerID?>> = Array(size) { Array(size) { null } }
+
+    fun isValidCoordinate(x: Int, y: Int): Boolean = (x in 0 until size) and (y in 0 until size)
+
+    /**
+     * Allows to write `varName[i, j]` to get cell in position `(i, j)`.
+     *
+     * Example:
+     *
+     * ```
+     * val board = GameBoard(5)
+     * val a = board[1, 1] // return cell in position (1, 1)
+     * ```
+     *
+     * To get cells inside methods of this class, use `this[i, j]`.
+     */
+    operator fun get(row: Int, col: Int) : PlayerID? = board.getOrNull(row)?.getOrNull(col)
+
+    /**
+     * Allows to write `varName[i, j] = ...` to set cell in position (i, j).
+     *
+     * Example:
+     * ```
+     * val board = GameBoard(5)
+     * board[1, 1] = PLAYER_1
+     * ```
+     *
+     * To set cells inside methods of this class, use `this[i, j] = ...`.
+     */
+    operator fun set(row: Int, col: Int, player: PlayerID) {
+        board[row][col] = player
+        // TODO this is unsafe, fix it!
+    }
+
+    fun freeCell(raw: Int, col: Int): Nothing = TODO()
+    fun getNeighbors(row: Int, col: Int): List<Pair<Int, Int>> = TODO()
+
+    fun countFriendlyNeighbors(row: Int, col: Int, player: PlayerID) : Int = TODO()
+    fun countFriendlyNeighborsCorners(row: Int, col: Int, player: PlayerID): Int = TODO()
+    fun countEnemyNeighbors(row: Int, col: Int, player: PlayerID): Int = TODO()
+
+    fun isSurroundedWithFriendly(row: Int, col: Int): Boolean = TODO()
+    fun isCorner(row: Int, col: Int): Boolean = TODO()
+
+    fun toJson(): String = Gson().toJson(this)
+    companion object {
+        fun fromJson(json: String): GameBoard = Gson().fromJson(json, GameBoard::class.java)
+    }
+}
---
+++ src/main/kotlin/com/delta/Tilous.kt
@@ -0,0 +1,79 @@
+package com.delta
+
+import com.google.gson.Gson
+
+class Tilous(private val board: GameBoard) {
+    private val playersResources = PlayerID.values().associateWith { 1 }.toMutableMap()
+    private val playersStates = PlayerID.values().associateWith { PlayerState.PLAYING }.toMutableMap()
+
+    private val nextPlayersMap: Map<PlayerID, PlayerID> = mapOf() //TODO change it!
+    var currentPlayer = PlayerID.PLAYER_1
+        private set
+    var gameIsOver = false
+        private set
+
+    /* TODO: Use "init" syntax here to place one cell for each player in the corners */
+
+    // Info about board
+    fun getCell(row: Int, col: Int): PlayerID? = board[row, col]
+    fun getBoardSize(): Int = board.size
+
+    // Info about players
+    fun getPlayerResources() = playersResources.toMap()
+    fun getPlayerStates() = playersStates.toMap()
+
+    fun getNextPlayer(): PlayerID = TODO()
+
+    fun getWinner(): PlayerID? = TODO()
+
+    // Game checks and info
+    fun isValidCellToPlace(row: Int, col: Int, player: PlayerID): Boolean = TODO()
+
+    fun isProductive(row: Int, col: Int, player: PlayerID): Boolean = TODO()
+    fun isProductive(row: Int, col: Int): Boolean = TODO()
+    fun isSuperStable(row: Int, col: Int): Boolean = TODO()
+
+    fun getDefencePoints(row: Int, col: Int): Int = TODO()
+
+    private fun countCellsWithCondition(condition: (Int, Int) -> Boolean): Int = TODO()
+    fun countProductiveCells(player: PlayerID): Int = TODO()
+    fun countFreeCells(): Int = TODO()
+    fun countFriendlyCells(player: PlayerID): Int = TODO()
+
+    fun getSuperStableCells(player: PlayerID): List<Pair<Int, Int>> = TODO("Later...")
+    fun getUnstableCells(player: PlayerID): List<Pair<Int, Int>> = TODO("Later...")
+    fun getStableCells(player: PlayerID): List<Pair<Int, Int>> = TODO("Later...")
+
+    /**
+     * This is a main function!
+     *
+     * Check if game is over
+     * If not -- check if player can place a cell here
+     * Don't forget about resources
+     * If everything is ok, place the cell AND update everything:
+     * player's resources, board state and so on.
+     * Use already implemented methods.
+     *
+     *
+     * @return 'true' is the cell was successfully placed, 'false' if not
+     */
+    fun placeCell(row: Int, col: Int, player: PlayerID): Boolean = false
+
+    /**
+     * Another main function!
+     *
+     * Handles request of a player to finish theirs turn.
+     * We have to check here if the game was over,
+     * update resources and
+     * change current player
+     *
+     * @return 'true' if succeeded
+     */
+    fun finishPlayersTurn(player: PlayerID): Boolean = false
+
+    // Internal game actions
+    private fun removeUnstableCells(): Nothing = TODO()
+    private fun updatePlayerStates(): Nothing = TODO()
+
+    fun toJson() = Gson().toJson(this)
+}
