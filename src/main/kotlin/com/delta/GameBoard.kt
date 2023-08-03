66d36c2fa72b5e6d81103d77127366f4858df0e1
George Petviashvili <george@gnom.us>
2023-08-03 10:10:46 +0200

    Added Documentation. Fixed isSurroundedByFriendlies.
    GameBoard.kt has been tested and works as Intended.

--- src/main/kotlin/com/delta/GameBoard.kt
+++ src/main/kotlin/com/delta/GameBoard.kt
@@ -33,14 +33,14 @@ class GameBoard(val size: Int) {
      * To set cells inside methods of this class, use `this[i, j] = ...`.
      */
     operator fun set(row: Int, col: Int, player: PlayerID) {
-        if (isValidCoordinate(row, col)){
+        if (isValidCoordinate(row, col)){  // Works as intended
             board[row][col] = player
         } else {
             throw IllegalArgumentException("Can't place here.") // TODO: Add handling to this exception
         }
     }
 
-    private fun freeCell(raw: Int, col: Int) : Boolean = get(raw, col) == null
+    private fun freeCell(raw: Int, col: Int) : Boolean = get(raw, col) == null //Works as intended
     fun getNeighbors(row: Int, col: Int): List<Pair<Int, Int>> {
         return listOf(
             Pair(row - 1, col),
@@ -50,7 +50,9 @@ class GameBoard(val size: Int) {
         ).filter { (x, y) -> isValidCoordinate(x, y) }
     }
 
-    fun getAllNeighbors(row: Int, col: Int): List<Pair<Int, Int>> {
+ /** Returns a list of all Neighbors including corners that are valid.*/
+    fun getCornersNeighbors(row: Int, col: Int): List<Pair<Int, Int>> { //Works as intended
+
         return listOf(
             Pair(row - 1, col),
             Pair(row + 1, col),
@@ -62,19 +64,31 @@ class GameBoard(val size: Int) {
             Pair(row + 1, col - 1),
         ).filter { (x, y) -> isValidCoordinate(x, y) }
     }
-
+/**
+ * Returns a number of friendly neighbors.
+ * */
     fun countFriendlyNeighbors(row: Int, col: Int, player: PlayerID) : Int {
         return getNeighbors(row, col).count { (x, y) -> get(x, y) == player }
     }
+    /**
+     * Returns a number of all friendly neighbors.
+     * */
     fun countFriendlyNeighborsCorners(row: Int, col: Int, player: PlayerID): Int {
-        return getAllNeighbors(row, col).count { (x, y) -> get(x, y) == player}
+        return getCornersNeighbors(row, col).count { (x, y) -> get(x, y) == player}
     }
+    /**
+     * Returns a number of enemy neighbors on the borders of this tile.
+     * */
     fun countEnemyNeighbors(row: Int, col: Int, player: PlayerID): Int{
         return getNeighbors(row, col).count { (x, y) -> get(x, y) != player && get(x, y) != null}
     }
+    /**
+     * Returns a boolean if this tile is surrounded by friendly tiles.
+     * */
 
     fun isSurroundedWithFriendly(row: Int, col: Int, player: PlayerID): Boolean {
-        return countFriendlyNeighborsCorners(row, col, player ) == 8
+        // Needed to add PlayerID to this function, WORKS
+        return countFriendlyNeighborsCorners(row, col, player) == getCornersNeighbors(row, col).size
     }
     fun isCorner(row: Int, col: Int): Boolean {
         return (row == 0 && col == 0) ||
