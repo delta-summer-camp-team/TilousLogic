532d2047d5eac52d429970c006c85c893a1952d3
lisa01234 <lisa.t.stepanova@gmail.com>
2023-08-05 14:46:53 +0200

    merge master

--- src/main/kotlin/com/delta/GameBoard.kt
+++ src/main/kotlin/com/delta/GameBoard.kt
@@ -51,4 +51,12 @@ class GameBoard(val size: Int) {
     companion object {
         fun fromJson(json: String): GameBoard = Gson().fromJson(json, GameBoard::class.java)
     }
+    fun ableToPlaceCell(row: Int, col: Int, player: PlayerID, resurses: Int) {
+        val friendlyNeighbours = countFriendlyNeighbors(row, col, player)
+        if friendlyNeighbours > 0 {
+            getCellColor(row: Int, col: Int)
+        } else {
+            return "isNotAbleToPlaceCell"
+        }
+    }
 }
